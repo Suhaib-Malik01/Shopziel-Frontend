2d4b468e48085a5d6689aeda9eca4fac0998f5e0
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-05-23 23:05:02 +0530

    added footer component

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,63 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Carousel from "../Components/Homepage/Carousel/Carousel";
+
 import Navbar from "../Components/Navbar";
-import ProductSlider from "../Components/Products/ProductSlider";
-import { Divider } from "@chakra-ui/react";
-import CategoryBox from "../Components/Homepage/Category/CategoryBox";
 import SignUp from "../Components/UserAuth/SignUp";
 import Login from "../Components/UserAuth/Login";
-
-const Homepage = () => {
-  const [productArr, setProductArr] = useState([]);
-
-  const fetchData = async () => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    try {
-      const response = await fetch(
-        "http://localhost:8888/api/products/",
-        requestOptions
-      );
-      let data = await response.json();
-      setProductArr(data);
-    } catch (error) {
-      console.log("An error occurred:", error);
-    }
-  };
-
-  
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <Carousel />
-      <ProductSlider
-        data={productArr ? productArr : []}
-        title={"New Arrivals."}
-        subTitle={"Gadgets & Laptop"}
-      />
-
-      <ProductSlider
-        data={productArr ? productArr : []}
-        title={"Discover more."}
-        subTitle={"Good things are waiting for you"}
-      />
-
-      <Divider w={"80%"} m={"auto"} my={"4rem"} />
-
-      <CategoryBox />
-    </>
-  );
-};
-
-
-
+import Footer from "../Components/Footer";
+import Homepage from "./Homepage";
 
 const AllRoutes = () => {
   return (
@@ -68,6 +16,7 @@ const AllRoutes = () => {
         <Route path="/Signin" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
