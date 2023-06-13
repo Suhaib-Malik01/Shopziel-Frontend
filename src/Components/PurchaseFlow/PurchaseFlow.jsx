a79d40c4e92e78b6d11cd8d78555baa7ef68442b
Suhaib Malik <suhaibmalik6397@gmail.com>
2023-06-13 23:25:42 +0530

    updated homepage

--- src/Components/PurchaseFlow/PurchaseFlow.jsx
+++ src/Components/PurchaseFlow/PurchaseFlow.jsx
@@ -1,15 +1,35 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Grid, GridItem } from '@chakra-ui/react';
 import React from 'react';
 import PurchaseFlowCard from './PurchaseFlowCard';
 
 const PurchaseFlow = () => {
+
+  let data = [{
+    img : ""
+  }]
+
   return (
-    <Flex w={"80%"} gap={"5"} m={"auto"} justifyContent={"center"}>
+    <Grid w={"80%"} gap={"5"} m={"auto"} justifyContent={"center"} templateColumns={[
+      "1fr",
+      "repeat(2, 1fr)",
+      "repeat(2, 1fr)",
+      
+      "repeat(4,1fr)",
+    ]}>
+      <GridItem>
       <PurchaseFlowCard />
+      </GridItem>
+      <GridItem>
       <PurchaseFlowCard />
+      </GridItem>
+      <GridItem>
       <PurchaseFlowCard />
+      </GridItem>
+      <GridItem>
       <PurchaseFlowCard />
-    </Flex>
+      </GridItem>
+      
+    </Grid>
   );
 }
 
